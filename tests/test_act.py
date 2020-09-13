605ea219c1cdbede0675be5a0a392290f32af549
Janluke <gianlucacaronte@gmail.com>
2020-09-13 14:45:55 +0200

    update manual test too

--- tests/test_act.py
+++ tests/test_act.py
@@ -2,35 +2,22 @@ try:
     from . import context
 except:
     import context 
-from pymugen.formats.act import from_file
 from pymugen.formats import Act
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
 
 import unittest as ut
 import os 
 
 def plot_palette(palette):
+    import matplotlib.pyplot as plt
+    from matplotlib.colors import ListedColormap
     tmp = np.array([n for n in range(256)]).reshape(16,16)
     plt.imshow(tmp,cmap=ListedColormap(palette/255))
+    plt.show()
 
 if __name__ == "__main__":
     with Act("tests/test_data/G6_Luffy/1.act") as act:
-        print(act[0])
-        print(act[1])
-        print(act[1:5])
-        print([a for a in act])
-    exit(0)
-    p = from_file("tests/test_data/G6_Luffy/1.act")
-    fname = "tests/test_data/G6_Luffy/{0}.act"
-    fig = plt.figure()
-    fig.canvas.set_window_title("Luffy palettes")
-    for n in range(1,7):
-        plt.subplot(3,2,n)
-        palette = from_file(fname.format(n))
-        plot_palette(palette)
-    plt.show()
+        plot_palette(act._data)
 
 class ActReading(ut.TestCase):
     fname_test = "tests/test_data/test.save.act"
@@ -70,7 +57,7 @@ class ActReading(ut.TestCase):
         with Act(self.fname_test) as act:
             act[:] = ((42, 42, 42),)*256
             act.save()
-            
+
         with Act(self.fname_test) as act:
             self.assertTupleEqual(act[0], (42, 42, 42))
             self.assertTupleEqual(act[42], (42, 42, 42))
